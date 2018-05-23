103a62eeb7bbf856f2d229203df457896195b528
Philipp Henkel <philipp.henkel@gmail.com>
2018-05-23 23:39:12 +0200

    Log requests to console

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const http = require('http');
+const port = 8080;
 
 const server = http.createServer(
   (request, response) => {
+    console.info(`${request.method} ${request.url}`)
     response.writeHead(200, {
       "Content-Type": "text/plain"
     });
     response.end("Hello, World!\n");
   });
 
-server.listen(8080);
+console.info(`Listen on port ${port}`)
+server.listen(port);
